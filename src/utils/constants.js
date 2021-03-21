972b94e0863a72ac62489ebdb4fd1e70c35a3c09
fatai balogun <fattylee.remod@gmail.com>
2021-03-21 13:09:23 +0100

    style the sing page component

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 export const MOST_POPULAR = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=1&api_key=${process.env.REACT_APP_MOVIE_API}`;
 
-export const POSTER_PATH = "https://image.tmdb.org/t/p/w500";
+export const POSTER_PATH = "https://image.tmdb.org/t/p/w342";
+
+export const BACKDROP_PATH_SIZE = "https://image.tmdb.org/t/p/w1280";
 
 export const SEARCH_MOVIE = `https://api.themoviedb.org/3/search/movie?&api_key=${process.env.REACT_APP_MOVIE_API}&query=`;
